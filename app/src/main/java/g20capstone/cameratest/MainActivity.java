9eb5210a16b9e8674e00caf9cd98eca6810784e4
Drew Barclay <drew.w.barclay@gmail.com>
2017-02-15 04:09:11 -0600

    Initial commit. Working camera display.

---
+++ app/src/main/java/g20capstone/cameratest/MainActivity.java
@@ -0,0 +1,123 @@
+package g20capstone.cameratest;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int PERMISSIONS_REQUEST_CAMERA = 42;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SurfaceView sv = (SurfaceView)findViewById(R.id.surfaceView);
+        final Activity thisActivity = this;
+        sv.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                System.out.println("Surface created.");
+                if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    setupCamera();
+                } else {
+                    ActivityCompat.requestPermissions(thisActivity, new String[]{Manifest.permission.CAMERA}, PERMISSIONS_REQUEST_CAMERA);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+}
+
+    protected void setupCamera() {
+        try {
+            CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String camId = manager.getCameraIdList()[0];
+
+            CameraDevice.StateCallback callback = new CameraDevice.StateCallback() {
+
+                @Override
+                public void onOpened(final CameraDevice camera) {
+                    try {
+                        SurfaceView sv = (SurfaceView)findViewById(R.id.surfaceView);
+                        final Surface surface = sv.getHolder().getSurface();
+                        camera.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+                            @Override
+                            public void onConfigured(CameraCaptureSession session) {
+                                try {
+                                    CaptureRequest.Builder builder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                                    builder.addTarget(surface);
+                                    session.setRepeatingBurst(Arrays.asList(builder.build()), null, null);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onConfigureFailed(CameraCaptureSession session) {
+
+                            }
+                        }, null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onDisconnected(CameraDevice camera) {
+
+                }
+
+                @Override
+                public void onError(CameraDevice camera, int error) {
+
+                }
+            };
+
+            try {
+                manager.openCamera(camId, callback, null);
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setupCamera();
+                }
+            }
+        }
+    }
+}
