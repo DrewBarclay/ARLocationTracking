bf1745005f9d9f08967bb6eeeb0d021c087bc5f1
Drew Barclay <drew.w.barclay@gmail.com>
2017-02-16 02:56:41 -0600

    Figured out how to draw a triangle. Will see if I can move the camera based on orientation next.

---
+++ app/src/main/java/g20capstone/cameratest/ARRenderer.java
@@ -0,0 +1,76 @@
+package g20capstone.cameratest;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.util.Log;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by Drew on 2/16/2017.
+ */
+
+public class ARRenderer implements GLSurfaceView.Renderer {
+    private Triangle mTriangle;
+
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        mTriangle = new Triangle();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // Redraw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        mTriangle.draw(mMVPMatrix);
+    }
+
+    //Code copied from Google's sample code.
+    public static int loadShader(int type, String shaderCode){
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+
+    //Copied from Google's sample code
+    public static void checkGlError(String glOperation) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e("OpenGL", glOperation + ": glError " + error);
+            throw new RuntimeException(glOperation + ": glError " + error);
+        }
+    }
+}
--- app/src/main/java/g20capstone/cameratest/MainActivity.java
+++ app/src/main/java/g20capstone/cameratest/MainActivity.java
@@ -49,23 +49,7 @@ public class MainActivity extends AppCompatActivity {
         mGLSurfaceView.setEGLContextClientVersion(2);
         mGLSurfaceView.setEGLConfigChooser(8,8,8,8,16,0);
         mGLSurfaceView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
-        mGLSurfaceView.setRenderer(new GLSurfaceView.Renderer() {
-            @Override
-            public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-                GLES20.glClearColor(0.0f, 0.0f, 1.0f, 0.3f);
-            }
-
-            @Override
-            public void onSurfaceChanged(GL10 gl, int width, int height) {
-                GLES20.glViewport(0, 0, width, height);
-            }
-
-            @Override
-            public void onDrawFrame(GL10 gl) {
-                // Redraw background color
-                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-            }
-        });
+        mGLSurfaceView.setRenderer(new ARRenderer());
 
         //Set up camera
         mSurfaceView = new SurfaceView(this);
